dfeabd8b70bc18f2261e879cedbdc1cb8580d78e
razikdontcare <gamersia4@gmail.com>
2025-01-23 13:49:59 +0800

    fix: update canvas dependency to @napi-rs/canvas and clean up code formatting

--- models/BaliLocationOfDoor.js
+++ models/BaliLocationOfDoor.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const { loadImage, createCanvas } = require("canvas");
+const { loadImage, createCanvas } = require("@napi-rs/canvas");
 
 class BaliLocationOfDoor {
   getPhilosophy(direction) {
@@ -222,117 +222,115 @@ class BaliLocationOfDoor {
         break;
     }
 
-        // Load image dari path static backend
-        const image = await loadImage(`http://localhost:9000${urlImg}`);
+    // Load image dari path static backend
+    const image = await loadImage(`http://localhost:9000${urlImg}`);
 
-        context.drawImage(image, 0, 0, width, height);
-        let x, y;
-        const text = `${length.toFixed(1)} m`;
-        context.fillStyle = "#000000";
-        context.font = "bold 16pt Menlo";
+    context.drawImage(image, 0, 0, width, height);
+    let x, y;
+    const text = `${length.toFixed(1)} m`;
+    context.fillStyle = "#000000";
+    context.font = "bold 16pt Menlo";
 
-        switch (direction) {
-          case "utara":
-            const divison = parseFloat(length);
+    switch (direction) {
+      case "utara":
+        const divison = parseFloat(length);
 
-            x = 3;
-            y = 120;
-            const initialValue = 0;
-            context.fillText(initialValue.toFixed(0), x, y);
-            console.log(`Current value: ${initialValue.toFixed(0)}`);
+        x = 3;
+        y = 120;
+        const initialValue = 0;
+        context.fillText(initialValue.toFixed(0), x, y);
+        console.log(`Current value: ${initialValue.toFixed(0)}`);
 
-            for (let i = 0; i < 9; i++) {
-              const currentValue = ((i + 1) * divison).toFixed(2);
-              console.log(`Current value: ${currentValue}`);
+        for (let i = 0; i < 9; i++) {
+          const currentValue = ((i + 1) * divison).toFixed(2);
+          console.log(`Current value: ${currentValue}`);
 
-              x = 70 + i * 96; 
-              y = 120; 
-              context.textAlign = "center";
-              context.fillText(currentValue, x, y);
-            }
-            break;
-
-          case "timur":
-            const divisiontimur = parseFloat(length); 
-            
-            x = 730;
-            y = 20;
-            const initialValuet = 0;
-            context.fillText(initialValuet.toFixed(0), x, y);
-            console.log(`Current value: ${initialValuet.toFixed(0)}`);
-      
-            for (let i = 0; i < 9; i++) {
-              const currentValue = ((i + 1) * divisiontimur).toFixed(2); 
-              console.log(`Current value: ${currentValue}`);
-
-              x = width - 100; 
-              y = 100 + i * 108; 
-              context.textAlign = "right";
-              context.fillText(currentValue, x, y);
-            }
-            break;
-
-          case "selatan":
-            const divisionselatan = parseFloat(length);
-
-            x = 835;
-            y = 895;
-            const initialValues = 0;
-            context.fillText(initialValues.toFixed(0), x, y);
-            console.log(`Current value: ${initialValues.toFixed(0)}`);
-
-            for (let i = 0; i < 9; i++) {
-              const currentValue = ((i + 1) * divisionselatan).toFixed(2); 
-              console.log(`Current value: ${currentValue}`);
-
-              x = width - (i + .63) * 96; // Posisi horizontal
-              y = height - 100; // Posisi vertikal tetap
-              context.textAlign = "center";
-              context.fillText(currentValue, x, y);
-            }
-            break;
-
-
-          case "barat":
-            const divisionbarat = parseFloat(length);
-
-            x = 100;
-            y = 990;
-            const initialValueb = 0;
-            context.fillText(initialValueb.toFixed(0), x, y);
-            console.log(`Current value: ${initialValueb.toFixed(0)}`);
-
-            for (let i = 0; i < 9; i++) {
-              const currentValue = ((i + 1) * divisionbarat).toFixed(2); 
-              console.log(`Current value: ${currentValue}`);
-
-              x = 100; // Posisi horizontal tetap
-              y = height - (i + .8) * 108; // Posisi vertikal
-              context.textAlign = "left";
-              context.fillText(currentValue, x, y);
-            }
-            break;
-
-          default:
-            // Jika arah tidak dikenal, tampilkan satu teks di tengah
-            x = width / 2;
-            y = height / 2;
-            context.textAlign = "center";
-            context.fillText(text, x, y);
-            break;
+          x = 70 + i * 96;
+          y = 120;
+          context.textAlign = "center";
+          context.fillText(currentValue, x, y);
         }
+        break;
 
-          // Log koordinat untuk debugging
-          console.log(`Direction: ${direction}, x: ${x}, y: ${y}`);
+      case "timur":
+        const divisiontimur = parseFloat(length);
 
-          const imgBuffer = canvas.toBuffer("image/png");
-    
-          return imgBuffer.toString("base64");
+        x = 730;
+        y = 20;
+        const initialValuet = 0;
+        context.fillText(initialValuet.toFixed(0), x, y);
+        console.log(`Current value: ${initialValuet.toFixed(0)}`);
 
+        for (let i = 0; i < 9; i++) {
+          const currentValue = ((i + 1) * divisiontimur).toFixed(2);
+          console.log(`Current value: ${currentValue}`);
+
+          x = width - 100;
+          y = 100 + i * 108;
+          context.textAlign = "right";
+          context.fillText(currentValue, x, y);
+        }
+        break;
+
+      case "selatan":
+        const divisionselatan = parseFloat(length);
+
+        x = 835;
+        y = 895;
+        const initialValues = 0;
+        context.fillText(initialValues.toFixed(0), x, y);
+        console.log(`Current value: ${initialValues.toFixed(0)}`);
+
+        for (let i = 0; i < 9; i++) {
+          const currentValue = ((i + 1) * divisionselatan).toFixed(2);
+          console.log(`Current value: ${currentValue}`);
+
+          x = width - (i + 0.63) * 96; // Posisi horizontal
+          y = height - 100; // Posisi vertikal tetap
+          context.textAlign = "center";
+          context.fillText(currentValue, x, y);
+        }
+        break;
+
+      case "barat":
+        const divisionbarat = parseFloat(length);
+
+        x = 100;
+        y = 990;
+        const initialValueb = 0;
+        context.fillText(initialValueb.toFixed(0), x, y);
+        console.log(`Current value: ${initialValueb.toFixed(0)}`);
+
+        for (let i = 0; i < 9; i++) {
+          const currentValue = ((i + 1) * divisionbarat).toFixed(2);
+          console.log(`Current value: ${currentValue}`);
+
+          x = 100; // Posisi horizontal tetap
+          y = height - (i + 0.8) * 108; // Posisi vertikal
+          context.textAlign = "left";
+          context.fillText(currentValue, x, y);
+        }
+        break;
+
+      default:
+        // Jika arah tidak dikenal, tampilkan satu teks di tengah
+        x = width / 2;
+        y = height / 2;
+        context.textAlign = "center";
+        context.fillText(text, x, y);
+        break;
+    }
+
+    // Log koordinat untuk debugging
+    console.log(`Direction: ${direction}, x: ${x}, y: ${y}`);
+
+    const imgBuffer = canvas.toBuffer("image/png");
+
+    return imgBuffer.toString("base64");
   }
 
   async getLocationOfDoor(yardLength, homeDirection) {
-    let length = yardLength/9;
+    let length = yardLength / 9;
 
     return {
       bytesImg: await this.getBytesImage(length, homeDirection),
@@ -343,5 +341,3 @@ class BaliLocationOfDoor {
 
 const baliLocationOfDoor = new BaliLocationOfDoor();
 module.exports = baliLocationOfDoor;
-
-
