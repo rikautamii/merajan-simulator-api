fe3cb42dc42d15f3a33c241c4cf12cdce200a479
razikdontcare <gamersia4@gmail.com>
2025-01-23 18:39:38 +0800

    fix: add URL parameter to image loading functions for dynamic hostname handling

--- controllers/locationDoorController.js
+++ controllers/locationDoorController.js
@@ -2,10 +2,20 @@ const LocationDoor = require("../models/BaliLocationOfDoor");
 
 const getLocationDoor = async (req, res) => {
   const { yardLength, landDirection } = req.body;
+  let url = "";
+  const hostname = req.hostname;
+  const protocol = req.protocol;
+
+  if (hostname === "localhost") {
+    url = `${protocol}://${hostname}:9000`;
+  } else {
+    url = `${protocol}://${hostname}`;
+  }
 
   const locationDoor = await LocationDoor.getLocationOfDoor(
     yardLength,
-    landDirection
+    landDirection,
+    url
   );
 
   res.status(200).json(locationDoor);
@@ -14,4 +24,3 @@ const getLocationDoor = async (req, res) => {
 module.exports = {
   getLocationDoor,
 };
-
