8c7f4515772cd1e7a1f0a871d3992e9b4ee654e2
rikautamii <utami.2308561023@student.unud.ac.id>
2025-07-02 22:50:22 +0800

    fix: remove trailing slash from production URL in getBuildingLocation method

--- controllers/buildingLocationController.js
+++ controllers/buildingLocationController.js
@@ -6,7 +6,7 @@ const getBuildingLocation = async (req, res) => {
   let url = "";
 
   if (process.env.NODE_ENV === "production") {
-    url = `https://merajan-simulator-api.vercel.app/`;
+    url = `https://merajan-simulator-api.vercel.app`;
     // url = `http://localhost:9000`;
   } else {
     url = `http://localhost:9000`;
